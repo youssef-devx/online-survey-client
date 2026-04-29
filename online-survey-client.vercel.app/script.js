dce1a84dd5c0a5a1de2aeda65488912bf94582fa
youssef-devx <youssefboulalq@gmail.com>
2026-04-29 19:13:10 +0100

    fix 1

--- online-survey-client.vercel.app/script.js
+++ online-survey-client.vercel.app/script.js
@@ -197,7 +197,7 @@ async function submitSurvey() {
   };
 
   try {
-    const res = await fetch('https://online-survey-serverless-function.vercel.app/', {
+    const res = await fetch('https://online-survey-serverless-function.vercel.app/api/survey', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload),
